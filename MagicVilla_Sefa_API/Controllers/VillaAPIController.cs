bb5d6b6f06aba8c49540f15368126a3250febd8e
sefa.maril <sefa.maril@tubitak.gov.tr>
2023-06-10 17:16:40 +0300

    Status Code in Endpoints

---
+++ MagicVilla_Sefa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,28 @@
+﻿using MagicVilla_Sefa_API.Data;
+using MagicVilla_Sefa_API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Sefa_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+                return NotFound();
+
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_Sefa_API/Models/Dto/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_Sefa_API.Models.Dto
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
